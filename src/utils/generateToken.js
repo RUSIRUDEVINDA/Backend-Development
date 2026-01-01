181abf52183810235062b521a6e4e1c3f0f19d0e
Rusiru Devinda <116636125+RUSIRUDEVINDA@users.noreply.github.com>
2026-01-01 22:39:05 +0530

    Create generateToken.js

---
+++ src/utils/generateToken.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+// Function to generate JWT token
+export const generateToken = (userID,res) => {
+    const payload = { id: userID };
+    const token = jwt.sign(payload,process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_EXPIRES_IN || "7d",
+    });
+
+ // Set the token in an HTTP-only cookie
+    res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",  //protect against CSRF attacks
+    maxAge: (1000 * 60 * 60 * 24) * 7,
+  });
+
+    return token;
+}
+
+
+
+//use this to generate random jwt secret -> node -e "console.log(require('crypto').randomBytes(32).toString('base64'))"
